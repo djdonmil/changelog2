0975c23addfecd3cb0c619dee78acf03649d2518
Vaibhav Patel <vaibhav.patel@MACPRO09.local>
2022-05-03 17:05:14 +0530

    normal commit

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 var fs = require(`fs`);
+var showdown = require('showdown')
+
 
 var template = fs.readFileSync(`CHANGELOG.md`, 'utf8');
-var showdown = require('showdown'),
     converter = new showdown.Converter(),
     text = template,
     html = converter.makeHtml(text);
-    console.log("==>>>", html)
 
     fs.writeFile('newfile.html', html, function (err) {
         if (err) throw err;
         console.log('File is created successfully.');
       });
+
+var QRCode = require('qrcode')
+
+for (let i = 1; i <= 1000; i++) {
+  const path = `./qrcodes/loba-${i}.png`
+  QRCode.toFile(path, (makeid(5) + new Date().getTime()).toString(), {
+    // color: {
+    //   dark: '#00F', // Blue modules
+    //   light: '#0000' // Transparent background
+    // }
+  }, function (err) {
+    if (err) throw err
+    console.log('saved.---',i)
+  })
+}
+function makeid(length) {
+  var result = '';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() *
+      charactersLength));
+  }
+  return result;
+}
