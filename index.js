77549fe270db95be45a4dc03ec1502c7b69ef192
Vaibhav Patel <vaibhav.patel@MACPRO09.local>
2022-05-03 17:14:18 +0530

    fix(new feature): ignore commit in tag

--- index.js
+++ index.js
@@ -12,27 +12,27 @@ var template = fs.readFileSync(`CHANGELOG.md`, 'utf8');
         console.log('File is created successfully.');
       });
 
-var QRCode = require('qrcode')
+// var QRCode = require('qrcode')
 
-for (let i = 1; i <= 1000; i++) {
-  const path = `./qrcodes/loba-${i}.png`
-  QRCode.toFile(path, (makeid(5) + new Date().getTime()).toString(), {
-    // color: {
-    //   dark: '#00F', // Blue modules
-    //   light: '#0000' // Transparent background
-    // }
-  }, function (err) {
-    if (err) throw err
-    console.log('saved.---',i)
-  })
-}
-function makeid(length) {
-  var result = '';
-  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  var charactersLength = characters.length;
-  for (var i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() *
-      charactersLength));
-  }
-  return result;
-}
+// for (let i = 1; i <= 1000; i++) {
+//   const path = `./qrcodes/loba-${i}.png`
+//   QRCode.toFile(path, (makeid(5) + new Date().getTime()).toString(), {
+//     // color: {
+//     //   dark: '#00F', // Blue modules
+//     //   light: '#0000' // Transparent background
+//     // }
+//   }, function (err) {
+//     if (err) throw err
+//     console.log('saved.---',i)
+//   })
+// }
+// function makeid(length) {
+//   var result = '';
+//   var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+//   var charactersLength = characters.length;
+//   for (var i = 0; i < length; i++) {
+//     result += characters.charAt(Math.floor(Math.random() *
+//       charactersLength));
+//   }
+//   return result;
+// }
